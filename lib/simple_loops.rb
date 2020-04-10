249801cc7a3c5d29c846ae73525e8dc9c23a36d0
Steven Ross <ross.steven@me.com>
2020-04-10 19:08:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,10 @@ def loop_message_n_times(string, number)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts output_array(counter)
+    counter += 1
+  end
